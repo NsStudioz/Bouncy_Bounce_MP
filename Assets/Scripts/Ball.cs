daf0f849c9a8488c3cbdaa73c6fcf68cdb48d7fb
NsStudioz <sneve2016@gmail.com>
2023-04-13 18:03:01 +0300

    Ball Bounce.

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))] // add the rigidbody component automatically on instantiation.
+public class Ball : MonoBehaviour
+{
+    [Header("Physics Settings")]
+    Rigidbody2D rigidBody2D;
+    //
+    [SerializeField] private float bounceVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.collider.TryGetComponent(out PlayerController playerController)) // if the collider has the component type of PlayerController...
+        {
+            Bounce(col.GetContact(0).normal);
+        }
+    }
+
+    private void Bounce(Vector2 normal)
+    {
+        rigidBody2D.velocity = normal * bounceVelocity;
+    }
+}
