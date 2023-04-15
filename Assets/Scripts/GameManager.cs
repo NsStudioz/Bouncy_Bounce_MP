002df26f13841ba28397f2864c3ca6777094143a
NsStudioz <sneve2016@gmail.com>
2023-04-15 11:54:19 +0300

    Game Manager with game states mechs

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+using System;
+
+public class GameManager : NetworkBehaviour
+{
+    public static GameManager instance;
+
+
+    public enum State
+    {
+        Menu,
+        Game,
+        Win,
+        Lose
+    }
+
+    private State gameState;
+    //
+    private int connectedPlayers;
+
+    [Header("Events")]
+    public static Action<State> OnGameStateChanged;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+
+        NetworkManager.OnServerStarted += NetworkManager_OnServerStarted;
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        base.OnNetworkDespawn();
+
+        NetworkManager.OnServerStarted -= NetworkManager_OnServerStarted;
+    }
+
+    private void NetworkManager_OnServerStarted()
+    {
+        if (!IsServer) // if this is NOT the host or server.
+            return;
+
+        // If it is the host or server, go here:
+        connectedPlayers++; // add to the variable an additional player.
+        NetworkManager.Singleton.OnClientConnectedCallback += Singleton_OnClientConnectedCallback; // subscribe
+    }
+
+    private void Singleton_OnClientConnectedCallback(ulong obj)
+    {
+        if (connectedPlayers >= 2) // if there are 2 players
+        {
+            StartGame(); // start the game.
+        }
+    }
+
+    private void StartGame()
+    {
+        StartGameClientRpc(); // make a ClientRpc call.
+    }
+
+    [ClientRpc]
+    private void StartGameClientRpc()
+    {
+        gameState = State.Game;
+        OnGameStateChanged?.Invoke(gameState);
+    }
+
+    void Start()
+    {
+        gameState = State.Menu;
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+
+
+
+}
